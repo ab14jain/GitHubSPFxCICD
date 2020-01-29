05fb70d9be66ddde726add0b6ee6b40c9660bea8
Abhishek Kumar Jain <abhishek.kj@dgsl.in>
2020-01-29 19:02:31 +0530

    updated office 365 cli bash script for incorrect command

--- devops/release/office-365-CLI-add-app.sh
+++ devops/release/office-365-CLI-add-app.sh
@@ -8,6 +8,6 @@ o365 version
 # you have to run "o365 spo login https://contoso.sharepoint.com"
 # to agree with the consent first time
 
-o365 spo login $SITE_URL --authType password --userName $EMAIL --password $PASS
+o365 login $SITE_URL --authType password --userName $EMAIL --password $PASS
 
 o365 spo app add --filePath "./_ab14jain.GitHubSPFxCICD/SPFx sppkg/cicd-git-hub.sppkg" --overwrite --verbose
