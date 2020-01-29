b2c5a2e953b53b5c98c59ccdd6e71390fbba13dc
Abhishek Kumar Jain <abhishek.kj@dgsl.in>
2020-01-29 17:46:13 +0530

    first commit

---
+++ src/webparts/gitHubCicd/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IGitHubCicdWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'GitHubCicdWebPartStrings' {
+  const strings: IGitHubCicdWebPartStrings;
+  export = strings;
+}
