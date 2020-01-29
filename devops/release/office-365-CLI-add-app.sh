9c4a966561d49c56f9d33cfbd8d5735645fd1c99
Abhishek Kumar Jain <abhishek.kj@dgsl.in>
2020-01-29 18:52:07 +0530

    updated office 365 cli bash script

--- devops/release/office-365-CLI-add-app.sh
+++ devops/release/office-365-CLI-add-app.sh
@@ -10,4 +10,4 @@ o365 version
 
 o365 spo login $SITE_URL --authType password --userName $EMAIL --password $PASS
 
-o365 spo app add --filePath "./_SPFx build/SPFx sppkg/react-jest-testing.sppkg" --overwrite --verbose
+o365 spo app add --filePath "./_ab14jain.GitHubSPFxCICD/SPFx sppkg/cicd-git-hub.sppkg" --overwrite --verbose
